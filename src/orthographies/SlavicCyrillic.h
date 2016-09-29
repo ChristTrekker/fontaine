3e8c1a3557a570818179df112edc870fb94b4fce
ChristTrekker <christtrekker@users.sourceforge.net>
2016-09-29 14:07:09 +0000

    Fix copyright year of new files.

--- src/orthographies/SlavicCyrillic.h
+++ src/orthographies/SlavicCyrillic.h
@@ -1,7 +1,7 @@
 //
 // The Fontaine Font Analysis Project 
 // 
-// Copyright (c) 2009 by Edward H. Trager
+// Copyright (c) 2016
 // All Rights Reserved
 // 
 // Released under the GNU GPL version 2.0 or later.
